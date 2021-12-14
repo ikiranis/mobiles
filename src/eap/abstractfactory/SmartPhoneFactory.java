5718302ebec97f851de9d219c5fc8a63379fbfb7
rocean <rocean74@gmail.com>
2021-12-14 22:52:22 +0200

    Smartphone factory

--- src/eap/abstractfactory/SmartPhoneFactory.java
+++ src/eap/abstractfactory/SmartPhoneFactory.java
@@ -1,4 +1,29 @@
 package eap.abstractfactory;
 
-//Η κλάση αυτή υλοποιεί τη διεπαφή PhoneAbstractFactory
+import java.awt.*;
+
+public class SmartPhoneFactory implements PhoneAbstractFactory {
+    private final int batterySize;
+    private final Dimension screenSize;
+    private final String phoneNumber;
+    private final String manufacturer;
+    private final int storage;
+    private final Camera camera;
+    private final String operatingSystem;
+
+    public SmartPhoneFactory(int batterySize, Dimension screenSize, String phoneNumber, String manufacturer, int storage, Camera camera, String operatingSystem) {
+        this.batterySize = batterySize;
+        this.screenSize = screenSize;
+        this.phoneNumber = phoneNumber;
+        this.manufacturer = manufacturer;
+        this.storage = storage;
+        this.camera = camera;
+        this.operatingSystem = operatingSystem;
+    }
+
+    @Override
+    public Phone createPhone() {
+        return new SmartPhone(batterySize, screenSize, phoneNumber, manufacturer, storage, camera, operatingSystem);
+    }
+}
 
